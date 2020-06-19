ef1630be28de859f6a8341ade7e3be658fcbb997
Sumit Kumar <sumitkmr93@outlook.com>
2020-06-19 23:32:21 +0100

    added format option

--- argumentparser.py
+++ argumentparser.py
@@ -5,5 +5,5 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square",help="display the square of the number given",type=int)
 args = parser.parse_args()
-print(args.square**2)
+print("The square of {} is {}".format(args.square,args.square**2))
 
