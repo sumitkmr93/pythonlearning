cf5f1da78d92c43a5e1a5be07bf06a3383a455c0
Sumit Kumar <sumitkmr93@outlook.com>
2020-06-19 23:25:41 +0100

    basic args value

--- argumentparser.py
+++ argumentparser.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import argparse
-print('Successfully imported')
 
-## comments added
+parser = argparse.ArgumentParser()
+parser.add_argument("square",help="display the square of the number given",type=int)
+args = parser.parse_args()
+print(args.square**2)
+
